ade932f26b46e68d71f8852a66c9fc320a57be8c
Ruben Feng <rubenfeng12@gmail.com>
2024-11-10 20:16:49 -0500

    agregando los modelos al API

---
+++ Proyecto-API-MHW/Models/MonstroGrandes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto_API_MHW.Models
+{
+    [Table("monstro_grande")]
+    public class MonstroGrandes
+    {
+        [Key]
+        public int id_monstrog { get; set; }
+        public string nombre { get; set; }
+        public int vida { get; set; }
+        public int id_categoria { get; set; }
+    }
+}
