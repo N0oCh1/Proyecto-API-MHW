b2ade5611e33da4542fccd05df605dccb53d127d
Ruben Feng <rubenfeng12@gmail.com>
2024-11-10 20:17:03 -0500

    agregando el controlador

---
+++ Proyecto-API-MHW/Controllers/MonstroGrandeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_API_MHW.Contexts;
+using Proyecto_API_MHW.Models;
+
+namespace Proyecto_API_MHW.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class MonstroGrandeController : ControllerBase
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public MonstroGrandeController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+        [HttpGet("mg")]
+        public async Task<ActionResult<List<vMonstroGrandes>>> GetMG()
+        {
+           return await applicationDbContext.vMonstroGrandes.ToListAsync();
+        }
+    }
+}
