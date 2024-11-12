70f90de06d54b546f65302955489b2d005c10f3d
Ruben Feng <rubenfeng12@gmail.com>
2024-11-12 18:47:49 -0500

    Agregando clases de datos para el correcto funcionamiento

---
+++ Proyecto-API-MHW/DataClass/DtaDebilidad.cs
@@ -0,0 +1,9 @@
+﻿namespace Proyecto_API_MHW.DataClass
+{
+    public class DtaDebilidad
+    {
+        public int id_elemento {  get; set; }
+        public string elemento { get; set; }
+        public double eficacia { get; set; }
+    }
+}
---
+++ Proyecto-API-MHW/DataClass/DtaItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Proyecto_API_MHW.DataClass
+{
+    public class DtaItem
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string description { get; set; } 
+    }
+}
