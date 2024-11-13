a6e85e7653196aa560d0635a416ed0c0bc1f0262
Ruben Feng <rubenfeng12@gmail.com>
2024-11-13 15:46:06 -0500

    Refactory: cambiando estructura del controlador y contexto

--- Proyecto-API-MHW/Controllers/MonstroGrandeController.cs
+++ Proyecto-API-MHW/Controllers/MonstroGrandeController.cs
@@ -1,82 +1,94 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Proyecto_API_MHW.Contexts;
 using Proyecto_API_MHW.DataClass;
-using Proyecto_API_MHW.Models;
-using Proyecto_API_MHW.Service;
-using System.Security.Cryptography.X509Certificates;
 
 namespace Proyecto_API_MHW.Controllers
 {
+    [Route("monstro")]
     [ApiController]
-    [Route("api/mg")]
     public class MonstroGrandeController : ControllerBase
     {
-        private readonly ApplicationDbContext applicationDbContext;
-        public MonstroGrandeController(ApplicationDbContext applicationDbContext)
+        private readonly MhwApiContext MhwApi;
+
+        public MonstroGrandeController(MhwApiContext context)
         {
-            this.applicationDbContext = applicationDbContext;
+            this.MhwApi = context;
         }
-        //[HttpGet("getTest")]
-        //public async Task<ActionResult<List<MonstroGrandes>>> getmgtest()
-        //{
-        //    return await applicationDbContext.MonstroGrandes.Include(e=> e.categorias).ToListAsync();   
-        //}
-        [HttpGet("get")]
-        public async Task<ActionResult<List<DtamonstroGrande>>> GetMonstroGrandes()
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<DtomonstroGrande>>> GetMonstro(int id)
         {
-            try
-            {
-                List<MonstroGrande> mg = await applicationDbContext.MonstroGrandes.ToListAsync();
-                List<Categoria> categoria = await applicationDbContext.Categorias.ToListAsync();
-                List<Bioma> bioma = await applicationDbContext.Biomas.ToListAsync();
-                List<MgBioma> mgBioma = await applicationDbContext.MgBiomas.ToListAsync();
-                List<Item> items = await applicationDbContext.items.ToListAsync();
-                List<Elemento> elementos = await applicationDbContext.elementos.ToListAsync();
-                List<MgElemento> mgElementos = await applicationDbContext.mgElementos.ToListAsync();
-                List<MgDebilidad> mgDebilidads = await applicationDbContext.mgDebilidades.ToListAsync();
-                List<Rango> rangos = await applicationDbContext.rangos.ToListAsync();
-                List<MgRango> mgRangos = await applicationDbContext.mgRangos.ToListAsync();
+            List<DtomonstroGrande> response = await MhwApi.MonstroGrandes
 
+                .AsSplitQuery()
+                .Include(m => m.IdCategoriaNavigation)
+                .Include(m => m.IdBiomas)
+                .Include(m => m.IdElementos)
+                .Include(m => m.IdImagenNavigation)
+                .Include(m => m.IdRangos)
+                .Include(m => m.Items)
+                .Include(m => m.MgDebilidades)
+                .ThenInclude(e => e.IdElementoNavigation)
+                .Select(monstro =>
+                    new DtomonstroGrande()
+                    {
+                        id = monstro.IdMonstrog,
+                        name = monstro.Nombre ?? string.Empty,
+                        health = monstro.Vida ?? int.MinValue,
+                        monsterClass = monstro.IdCategoriaNavigation.Tipo,
+                        image = new DtoImagen()
+                        {
+                            imageUrl = monstro.IdImagenNavigation.ImageUrl,
+                            iconUrl = monstro.IdImagenNavigation.IconUrl
+                        },
+                        location = monstro.IdBiomas.Select(b => b.NombreBioma).ToList(),
+                        range = monstro.IdRangos.Select(r => r.Rango1).ToList(),
+                        elements = monstro.IdElementos.Select(e => new DtoElemento()
+                        {
+                            id_elemento = e.IdElemento,
+                            elemento = e.Elemento1
+                        }).ToList(),
+                        weekness = monstro.MgDebilidades.Select(d => new DtoDebilidad()
+                        {
+                            id_elemento = d.IdElemento,
+                            elemento = d.IdElementoNavigation.Elemento1,
+                            eficacia = (double)d.Eficacia,
+                        }).ToList(),
+                        items = monstro.Items.Select(i => new DtoItem()
+                        {
+                            id = i.IdItem,
+                            name = i.NombreItem,
+                            description = i.DescripcionItem
+                        }).ToList(),
 
-                GetDataMG Getdata = new GetDataMG(
-                    mg,
-                    categoria,
-                    bioma,
-                    mgBioma,
-                    rangos,
-                    mgRangos,
-                    elementos,
-                    mgElementos,
-                    mgDebilidads,
-                    items
-                    );
-                List<DtamonstroGrande> response = Getdata.GetMonstro();
-                return Ok(response);
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500, "Ups, Ocurrio un erro");
-            }
+                    }
+                )
+                .AsNoTracking()
+                .ToListAsync();
+            return Ok(response.Find(x => x.id == id));
         }
-       
 
-        [HttpPost("post")]
-        public async Task<ActionResult<MonstroGrande>> postMG(MonstroGrande data)
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<DtoMonstroPreview>>> GetPreview()
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            try
-            {
-                applicationDbContext.MonstroGrandes.Add(data);
-                await applicationDbContext.SaveChangesAsync();
-                return CreatedAtAction(nameof(postMG), new { id = data.id_monstrog }, data);
-            }catch (DbUpdateException)
-            {
-                return StatusCode(500, "Error for create Object");
-            }
+            return Ok(await MhwApi.MonstroGrandes
+                .AsSplitQuery()
+                .Include(m => m.IdImagenNavigation)
+                .Select(monstro => new DtoMonstroPreview
+                {
+                    idMonstro = monstro.IdMonstrog,
+                    name = monstro.Nombre,
+                    iconImage = monstro.IdImagenNavigation.IconUrl,
+                    url = $"https://localhost:7101/monstro/get/{monstro.IdMonstrog}" 
+                })
+                .AsNoTracking()
+                .ToListAsync()
+                );
         }
     }
 }
