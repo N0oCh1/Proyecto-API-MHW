a2747d4f3e1fb0c9fb71a37879bd7cd30cb3c7af
Ruben Feng <rubenfeng12@gmail.com>
2024-11-16 14:28:08 -0500

    Datos y model

---
+++ Proyecto-API-MHW/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Proyecto_API_MHW.Models;
+
+public partial class Usuario
+{
+    public int Idusuario { get; set; }
+
+    public string? Nombreusuario { get; set; }
+
+    public string? Password { get; set; }
+}
